f62193d919c1251bd4523b39f4c054fae4c37af8
alam-anwar <93105876+alam-anwar@users.noreply.github.com>
2023-09-19 12:29:25 -0400

    basic plane rotation

---
+++ Assets/RotateInCircle.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class RotateInCircle : MonoBehaviour
+{
+
+    public GameObject plane;
+    public GameObject island;
+    public float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(island.transform.position, island.transform.up, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, island.transform.position, 1f);
+    }
+}

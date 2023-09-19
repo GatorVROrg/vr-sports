87da89a312befc3bebcd64a9ce6a7d920fbc2882
Bruno <bruno.v.dede@gmail.com>
2023-09-19 17:27:25 -0400

    Added velocity for plane object, now moves in a direction according to a force.

--- Assets/RotateInCircle.cs
+++ Assets/RotateInCircle.cs
@@ -6,20 +6,22 @@ using UnityEngine.UIElements;
 public class RotateInCircle : MonoBehaviour
 {
 
-    public GameObject plane;
     public GameObject island;
     public float speed = 10f;
+    public float forwardMotion = 10f;
+    Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(0, 0, 50f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(island.transform.position, island.transform.up, speed * Time.deltaTime);
-        transform.position = Vector3.MoveTowards(transform.position, island.transform.position, speed * Time.deltaTime);
+        //transform.RotateAround(island.transform.position, island.transform.up, speed * Time.deltaTime);
+        //transform.position = Vector3.MoveTowards(transform.position, island.transform.position, speed * Time.deltaTime);
     }
 }

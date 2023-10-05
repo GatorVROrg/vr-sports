9a2e5ff16f384cbee5f8c2ec6dc955aba54ab3a7
Bruno <bruno.v.dede@gmail.com>
2023-10-05 11:02:06 -0400

    Added Animation Keyframes
    I added animation keyframes for the plane, rotates around a "circle." Might be more smooth if a C# script was written that allows an object to rotate around a position, one that does not use Rigidbody.

--- Assets/RotateInCircle.cs
+++ Assets/RotateInCircle.cs
@@ -11,17 +11,95 @@ public class RotateInCircle : MonoBehaviour
     public float forwardMotion = 10f;
     Rigidbody rb;
 
+    float zVelocity = 10f;
+    float xVelocity = 70f;
+
+    float theta = 0f;
+    float distanceFromOrigin = 90f;
+
+    bool xIncreasing = true;
+    bool zIncreasing = true;
+
+    bool startDescent = false;
+
+    float originalYPos = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.velocity = new Vector3(0, 0, 50f);
+        //rb.velocity = new Vector3(0, 0, zVelocity);
+        //originalYPos = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.RotateAround(island.transform.position, island.transform.up, speed * Time.deltaTime);
-        //transform.position = Vector3.MoveTowards(transform.position, island.transform.position, speed * Time.deltaTime);
+        /*
+        Vector3 planeRectCoords = new Vector3(transform.position.x, originalYPos, transform.position.z);
+
+        if (startDescent)
+        {
+            planeRectCoords = new Vector3(transform.position.x, transform.position.y + (70 * speed * Time.deltaTime), transform.position.z);
+        }
+
+        Vector3 islandCenter = new Vector3(0f, 0f, 0f);
+
+        Vector3 directionToIsland = islandCenter - planeRectCoords;
+        rb.AddForceAtPosition(directionToIsland, transform.position);
+
+        */
+
+        //// Polar Coordinate Implementation(?):
+        float theta = Mathf.Atan(transform.position.x / transform.position.z);
+        float distanceFromOrigin = Mathf.Sqrt(Mathf.Pow(transform.position.x, 2) + Mathf.Pow(transform.position.z, 2));
+        //transform.position = new Vector3(transform.position.x - (distanceFromOrigin * Mathf.Cos(theta) * Time.deltaTime), transform.position.y - (10 * speed * Time.deltaTime), transform.position.z - (distanceFromOrigin * Mathf.Sin(theta) * Time.deltaTime));
+
+        //rb.velocity = new Vector3(xVelocity/(speed * Time.deltaTime), 0, zVelocity/(speed * Time.deltaTime));
+
+        // Angular Velocity Implementation:
+        //float radiusDistance = Mathf.Sqrt(Mathf.Pow(transform.position.x, 2) + Mathf.Pow(transform.position.z, 2));
+        //float omegaRadians = Mathf.PI * 2f * speed * Time.deltaTime;
+        //float angVelocity = omegaRadians / radiusDistance;
+
+        //transform.position = new Vector3(transform.position.x, transform.position.y - (10 * speed * Time.deltaTime), transform.position.z);
+
+        // Mathf.Sqrt(radiusDistance - Mathf.Pow(transform.position.x, 2))
+
+
+        /*
+        if (transform.position.x == 0)
+        {
+            //xIncreasing = false;
+            transform.position = new Vector3(transform.position.x + (10 * speed * Time.deltaTime), transform.position.y - (10 * speed * Time.deltaTime), transform.position.z);
+        }
+        else if (transform.position.x < 0)
+        {
+            rb.velocity = new Vector3(xVelocity, 0, zVelocity + (10 * speed * Time.deltaTime));
+            //transform.position = new Vector3(transform.position.x + (10 * speed * Time.deltaTime), transform.position.y - (10 * speed * Time.deltaTime), transform.position.z);
+        }
+        else
+        {
+            rb.velocity = new Vector3(xVelocity, 0, zVelocity - (10 * speed * Time.deltaTime));
+            //transform.position = new Vector3(transform.position.x - (10 * speed * Time.deltaTime), transform.position.y - (10 * speed * Time.deltaTime), transform.position.z);
+        }
+
+        if (transform.position.z == 0)
+        {
+            rb.velocity = new Vector3(xVelocity, 0, zVelocity - (10 * speed * Time.deltaTime));
+            //transform.position = new Vector3(transform.position.x, transform.position.y - (10 * speed * Time.deltaTime), transform.position.z + (10 * speed * Time.deltaTime));
+        }
+        else if (transform.position.z < 0)
+        {
+            rb.velocity = new Vector3(xVelocity, 0, zVelocity + (10 * speed * Time.deltaTime));
+            //transform.position = new Vector3(transform.position.x, transform.position.y - (10 * speed * Time.deltaTime), transform.position.z + (10 * speed * Time.deltaTime));
+        }
+        else
+        {
+            rb.velocity = new Vector3(xVelocity, 0, zVelocity - (10 * speed * Time.deltaTime));
+            //transform.position = new Vector3(transform.position.x, transform.position.y - (10 * speed * Time.deltaTime), transform.position.z - (10 * speed * Time.deltaTime));
+        }
+        */
+        
     }
 }

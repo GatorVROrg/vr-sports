bcbc3af50f1d790839d6db28ac03d3558d52c006
Mileshaase <mileshaase.imperium@gmail.com>
2023-10-06 00:37:49 -0400

    fixedSpawner

--- Assets/Scripts/FootballSpawner.cs
+++ Assets/Scripts/FootballSpawner.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class FootballSpawner : MonoBehaviour
 {
     public GameObject Football;
-    public GameObject spawner;
     public void Start()
     {
+        GameObject spawner = GameObject.Find("FootballSpawner");
         Instantiate(Football, spawner.transform.position, spawner.transform.rotation);
     }
     public void SpawnNewFootball()
     {
-        Instantiate(Football, spawner.transform.position, spawner.transform.rotation);
+        Debug.Log("spawned football");
+        GameObject spawner = GameObject.Find("FootballSpawner");
+        Vector3 position = spawner.transform.position;
+        Instantiate(Football, position, spawner.transform.rotation);
     }
 }
